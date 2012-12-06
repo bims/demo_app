01a9e0e6bb6258a59b8405258e3dd0e5c45e5e69
bims <olivier.dalle@unice.fr>
2012-12-06 14:40:48 -0800

    Import initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '6bb5afbc92fa981eada1dc818f4f1ba1e6fbf1c4591db8abfe5b4ad872b0848b687fcbd47aedfde25d149fb6018e74eec357a4dee131376fb428553485371501'
